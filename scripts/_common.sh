4efe4e918a2d93a97a9ab3ff0350552ae176874b
Jimmy Monin <jimmy@monin.net>
2021-02-05 22:00:38 +0100

    Upgrade to upstream version 7.4.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # Debian package version for Grafana
-GRAFANA_VERSION="7.3.3"
+GRAFANA_VERSION="7.4.0"
 # dependencies used by the app
 pkg_dependencies="influxdb"
 
