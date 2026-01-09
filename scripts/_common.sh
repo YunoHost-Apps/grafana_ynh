1dfade57a4afde1b4db74aff4c7bce5f881a35be
ewilly <ewilly@free.fr>
2026-01-09 14:52:01 +0100

    Refractor and upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-#=================================================
-# PERSONAL HELPERS
-#=================================================
+log_file="/var/log/$app/${app%_*}.log"
 
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
+# Set permissions
+myynh_set_permissions () {
+	chown -R "$app": "$install_dir"
+	chmod -R u=rwx,g=rwx,o= "$install_dir"
 
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
+	mkdir -p "$data_dir/plugins"
+	chown -R "$app": "$data_dir"
+	chmod -R u=rwx,g=rwx,o= "$data_dir"
+
+	mkdir -p "/var/log/$app"
+	chown -R "$app": "/var/log/$app"
+	chmod u=rwx,g=rx,o=rw "/var/log/$app"
+}
